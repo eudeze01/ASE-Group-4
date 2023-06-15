849e82a8715a7e439a1341e424c66743ff9e657b
eudeze01 <118227490+eudeze01@users.noreply.github.com>
2023-06-15 19:13:05 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_url_path='', 
+            static_folder='web/static', 
+            template_folder='web/template');
+
+@app.route("/")
+def index():
+    return render_template("index.html");
+
+
